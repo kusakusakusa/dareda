006247bf96e624571b8136380feb6890de1aefa3
Vic-L <vljc17@gmail.com>
2018-09-14 21:17:42 +0300

    feat: validate unique emails for presignup trigger

--- README.md
+++ README.md
@@ -4,6 +4,18 @@ Microservice to send custom messages in HTML to AWS Cognito users.
 
 ## USAGE
 
+### VALIDATE UNIQUE EMAILS
+
+A handler to check for uniuqe emails for the case of having a `Cognito username` that is not the email. This is meant for use in the `PreSignup Lambda trigger` only.
+
+Often, we allow users to change email, but setting the email as `username` in `Cognito` means that even if they change their email, they can still login via their old email whih is their username.
+
+The alternative is to set `username` as some sort of id like UUID and allow login via email.
+
+However, Cognito allows duplicate emails with different UUID/`username` to sign up. To prevent this, [@bsdkurt came up with a solution](https://github.com/aws-amplify/amplify-js/issues/369#issuecomment-369933670) to use the `PreSignup Lambda trigger` to validate the uniqueness of emails.
+
+This lambda trigger is under the function name `validateUniqueEmails` in the `handler` export.
+
 ### SETUP
 
 1. Create an AWS user with programmatic access. Deployment will be using [named profiles](https://docs.aws.amazon.com/cli/latest/userguide/cli-multiple-profiles.html). Permissions of the AWS user TODO
--- handler.js
+++ handler.js
@@ -1,7 +1,10 @@
+const AWS = require('aws-sdk')
+const cognitoIdp = new AWS.CognitoIdentityServiceProvider({apiVersion: '2016-04-18'})
+
 const CustomMessage_AdminCreateUser = require('./htmls/CustomMessage_AdminCreateUser').CustomMessage_AdminCreateUser
 const CustomMessage_ForgotPassword = require('./htmls/CustomMessage_ForgotPassword').CustomMessage_ForgotPassword
 
-'use strict';
+'use strict'
 
 // with reference to StackOverflow, https://stackoverflow.com/questions/45479961/aws-cognito-verification-email-using-parameters
 module.exports.sendEmails = function(event, context, callback) {
@@ -13,8 +16,8 @@ console.log("event", event) // logging
 
     switch (event.triggerSource) {
       case "CustomMessage_AdminCreateUser":
-        event.response.smsMessage = "Welcome to the service. Your confirmation code is " + event.request.codeParameter;
-        event.response.emailSubject = `Welcome ${name}`;
+        event.response.smsMessage = "Welcome to the service. Your confirmation code is " + event.request.codeParameter
+        event.response.emailSubject = `Welcome ${name}`
         event.response.emailMessage = CustomMessage_AdminCreateUser(usernameParameter, codeParameter, name)
         break
       case "CustomMessage_ForgotPassword":
@@ -27,4 +30,35 @@ console.log("event", event) // logging
   // Customize messages for other user pools
 console.log("event.response", event.response) // logging
   callback(null, event)
-};
+}
+
+// https://github.com/aws-amplify/amplify-js/issues/369#issuecomment-369933670
+module.exports.validateUniqueEmails = function(event, context) {
+  console.log(JSON.stringify(event))
+
+  // check if email is already in use
+  if (event.request.userAttributes.hasOwnProperty('email')) {
+    const email = event.request.userAttributes.email
+    
+    const params = {
+      UserPoolId: event.userPoolId,
+      Filter: 'email = "' + email + '"',
+    }
+    
+    cognitoIdp.listUsers(params).promise()
+    .then (results => {
+      console.log(JSON.stringify(results))
+      // if the usernames are the same, dont raise and error here so that
+      // cognito will raise the duplicate username error
+      if (results.Users.length > 0 && results.Users[0].Username !== event.userName) {
+        console.log('Duplicate email address in signup. ' + email)
+        context.done(Error('A user with the same email address exists'))
+      }
+      context.done(null, event)
+    })
+    .catch (error => {
+      console.error(error)
+      context.done(error)      
+    })
+  }
+}
